99a47fa49d4dc16c2f861d2af3c99422e2c04daa
oliverbskau <oliverbskau@gmail.com>
2021-03-18 11:43:46 +0100

    kids, guardian + employee updated

--- src/com/company/Guardian.java
+++ src/com/company/Guardian.java
@@ -2,7 +2,51 @@ package com.company;
 
 public class Guardian {
 
-   
+private String firstname;
+private String lastname;
+private String email;
+private String phonenumber;
+
+
+    public Guardian(){
+        setFirstname(firstname);
+        setLastname(lastname);
+        setEmail(email);
+        setPhonenumber(phonenumber);
+    }
+
+
+    public String getFirstname(){
+        return firstname;
+    }
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPhonenumber(){
+        return phonenumber;
+    }
+    public void setPhonenumber(String phonenumber){
+        this.phonenumber = phonenumber;
+    }
+
+    public String toString(){
+        return "Navn: " + getFirstname() + " " + getLastname() + " | Email: " + getEmail() + " | Telefon: " + getPhonenumber();
+    }
 
 
 }
