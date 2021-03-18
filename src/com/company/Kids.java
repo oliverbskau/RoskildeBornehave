3bca67467f5ccda67d15c869c1f35484a1ca10ef
oliverbskau <oliverbskau@gmail.com>
2021-03-18 11:28:21 +0100

    kids klasse lavet

--- src/com/company/Kids.java
+++ src/com/company/Kids.java
@@ -1,5 +1,44 @@
 package com.company;
 
+import java.util.Date;
+
 public class Kids {
+
+    private String firstname;
+    private String lastname;
+    private Date dateOfBirth;
+
+    public Kids(){
+        setFirstname(firstname);
+        setLastname(lastname);
+        setDateOfBirth(dateOfBirth);
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+
+    public Date getDateOfBirth(){
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(Date dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String toString(){
+        return "Navn: " + getFirstname() + " " + getLastname() + " | Født: " + getDateOfBirth();
+    }
+
+
 
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -5,6 +5,7 @@ public class Menu {
     public void menu(){
 
 
+        
 
     }
 
